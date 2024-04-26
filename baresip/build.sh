b8069f19b4804398cdacbad2202fe4c6634d4366
tudor <tudor@swisstch.com>
2024-04-26 14:28:52 +0200

    multi-arch simplification; add 48000Hz 2ch ringtones

---
+++ baresip/build.sh
@@ -0,0 +1,1 @@
+docker buildx build --no-cache --build-arg IMAGE=ghcr.io/copiltembel/barsesip_docker/libre:v3.11.0  --build-arg VERSION=v3.11.0 --platform linux/amd64,linux/arm64 -t ghcr.io/copiltembel/barsesip_docker/baresip:v3.11.0  --push .
